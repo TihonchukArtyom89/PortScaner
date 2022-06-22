32ebe3f215c9b1f690424b361c59db8ddc4f27a4
Тихончук Артём <artyom_tihonchuk_work@protonmail.com>
2022-06-22 17:18:12 +0300

    Добавьте файлы проекта.

---
+++ PortScaner/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace PortScaner
+{
+    partial class main_win_form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.btn_scan = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tIPAddress = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(32, 31);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(50, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "IP адрес";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Порт с";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(460, 31);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(19, 13);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "по";
+            // 
+            // btn_scan
+            // 
+            this.btn_scan.Location = new System.Drawing.Point(657, 24);
+            this.btn_scan.Name = "btn_scan";
+            this.btn_scan.Size = new System.Drawing.Size(131, 23);
+            this.btn_scan.TabIndex = 3;
+            this.btn_scan.Text = "Сканировать";
+            this.btn_scan.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 59);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 473);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 552);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // tIPAddress
+            // 
+            this.tIPAddress.Location = new System.Drawing.Point(101, 26);
+            this.tIPAddress.Name = "tIPAddress";
+            this.tIPAddress.Size = new System.Drawing.Size(145, 20);
+            this.tIPAddress.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(320, 26);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(498, 26);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 8;
+            // 
+            // main_win_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 587);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.tIPAddress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btn_scan);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "main_win_form";
+            this.Text = "PortScanner";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button btn_scan;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox tIPAddress;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
